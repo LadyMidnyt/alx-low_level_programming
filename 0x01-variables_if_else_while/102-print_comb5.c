a3b2fe5648f6b0bd12855749c2fbc7a026c2b801
Lerato Ncwadi <you@LadyMidnyt.com>
2022-07-01 14:57:32 -0700

    ‘copy’

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,42 +1,79 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success)
- */
+ * main - Prints 3 combination of number
+ * Return: Always (Success)
+*/
 int main(void)
 {
-	int c, i, k, j;
 
-	for (c = 48; c <= 57; c++)
-	{
-		for (i = 48; i <= 57; i++)
-		{
-			for (k = 48; <= 57; k++)
-			{
-				for (j = 48; <= 57; j++)
-				{
-					if (((K + j) > (c + i) && k >= c ) || c < k)
+		int c, i, k, j;
+
+		for (c = 48; c <= 57; c++)
+
 					{
-						putchar(c);
-						putchar(i);
-						putchar(' ');
-						putchar(k);
-						putchar(j);
 
-						if (c + i + k + j == 227 && c == 57)
-						{
-							break;
-						}
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+								for (i = 48; i <= 57; i++)
+
+											{
+
+															for (k = 48; k <= 57; k++)
+
+																			{
+
+																								for (j = 48; j <= 57; j++)
+
+																													{
+
+																																			if (((k + j) > (c + i) &&  k >= c) || c < k)
+
+																																									{
+
+																																																putchar(c);
+
+																																																						putchar(i);
+
+																																																												putchar(' ');
+
+																																																																		putchar(k);
+
+																																																																								putchar(j);
+
+
+
+																																																																													if (c + i + k + j == 227 && c == 57)
+
+																																																																																			{
+
+																																																																																									break;
+
+																																																																																														}
+
+																																																																																		else
+
+																																																																																								{
+
+																																																																																														putchar(',');
+
+																																																																																																			putchar(' ');
+
+																																																																																																								}
+
+																																																																																							}
+
+																																							}
+
+																											}
+
+																	}
+
+									}
+
+
+
+				putchar('\n');
+
+
+
+					return (0);
+
 }
