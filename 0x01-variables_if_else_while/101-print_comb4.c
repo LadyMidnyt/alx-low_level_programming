337ad0a0c33c3ff0c4f0c131e50aa76bcbff284f
Lerato Ncwadi <you@LadyMidnyt.com>
2022-07-01 14:21:22 -0700

    ‘long’

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,73 +1,33 @@
 #include <stdio.h>
-
-
-
 /**
- *
- *   * main - Prints 3 combination of numbers
- *
- *     *
- *
- *       * Return: Always (Success)
- *
- *         */
-
+ * main - Prints 3 combination of numbers
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
+	int c, i, k;
 
-		int c, i, k;
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if (c < i && i < k)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(k);
 
-
-
-			for (c = '0'; c <= '9'; c++)
-
+					if (c != '7')
 					{
-
-								for (i = '0'; i <= '9'; i++)
-
-											{
-
-															for (k = '0'; k <= '9'; k++)
-
-																			{
-
-																								if (c < i && i < k)
-
-																													{
-
-																																			putchar(c);
-
-																																								putchar(i);
-
-																																													putchar(k);
-
-
-
-																																																		if (c != '7')
-
-																																																								{
-
-																																																															putchar(',');
-
-																																																																					putchar(' ');
-
-																																																																										}
-
-																																																						}
-
-																											}
-
-																	}
-
-									}
-
-
-
-				putchar('\n');
-
-
-
-					return (0);
-
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
