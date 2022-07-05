83433f0a625957ae750f796d5e9c3c5a0166dd97
Lerato Ncwadi <you@LadyMidnyt.com>
2022-07-05 12:20:21 -0700

    9c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -33,6 +33,16 @@ void times_table(void)
 					_putchar (' ');
 				}
 			}
+			else
+			{
+				_putchar ((c / 10) + '0');
+				_putchar ((c % 10) + '0');
+				if (b < 9)
+				{
+					_putchar (',');
+					_pucthar (' ');
+				}
+			}
 		}
 		_putchar ('\n');
 	}
