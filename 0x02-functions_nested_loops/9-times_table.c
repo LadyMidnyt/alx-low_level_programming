96c8ae4cdff920641aaa74fe4c12223db51ab0f6
Lerato Ncwadi <you@LadyMidnyt.com>
2022-07-05 12:32:33 -0700

    9now

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,15 +2,13 @@
 
 /**
  * times_table - prints tables
- * Return: Always 0.
+ * Returns: Always 0.
  */
-
 void times_table(void)
 {
 	int a;
 	int b;
 	int c;
-
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
@@ -31,16 +29,6 @@ void times_table(void)
 				{
 					_putchar (',');
 					_putchar (' ');
-				}
-			}
-			else
-			{
-				_putchar ((c / 10) + '0');
-				_putchar ((c % 10) + '0');
-				if (b < 9)
-				{
-					_putchar (',');
-					_pucthar (' ');
 				}
 			}
 		}
