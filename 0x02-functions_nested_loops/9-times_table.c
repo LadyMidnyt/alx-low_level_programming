11119ab98b2d66e82d8209826563f9bdfdcf8aee
Lerato Ncwadi <you@LadyMidnyt.com>
2022-07-05 12:40:05 -0700

    yay

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,6 +9,7 @@ void times_table(void)
 	int a;
 	int b;
 	int c;
+
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
@@ -25,6 +26,16 @@ void times_table(void)
 					_putchar (' ');
 					_putchar ((c % 10) + '0');
 				}
+				if (b < 9)
+				{
+					_putchar (',');
+					_putchar (' ');
+				}
+			}
+			else
+			{
+				_putchar ((c / 10) + '0');
+				_putchar ((c % 10) + '0');
 				if (b < 9)
 				{
 					_putchar (',');
