97554a84004cbcc30cd8695f71d5f4f0bb49755a
Lerato Ncwadi <you@LadyMidnyt.com>
2022-07-27 03:15:58 -0700

    malloc

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-0x05. C - Pointers, arrays and strings
+0x0C-more_malloc_free
--- main.h
+++ main.h
@@ -2,16 +2,10 @@
 #define MAIN_H
 
 int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-#endif /* MAIN_H */
+#endif
